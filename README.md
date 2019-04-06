804790612dce8b253920a2950dd5250640938de0
Abc <anadelaine.catalla@gmail.com>
2019-04-05 23:13:16 -0700

    Added wins &losses

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # unit-4-game
 
+CrystalsCollector Game
+
+Instructions:
+
 A game with 4 crystals and a random target score.
 Each crystal has a random value that will be generated each time you win or lose
 Clicking the crystal will give you a score
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@ var wins = 0;
 var lost = 0;
 //Create something to track your score
 var yourScore = 0;
-
+// Create number to guess
 function getnewRandomnumbers() {
   yourScore = 0;
   $("#yourScore").html("<h3> Your Score: </h3>" + yourScore);
@@ -18,17 +18,35 @@ function getnewRandomnumbers() {
   //Go through each crystal image class
   $(".crystal-image").each(function() {
     //Each crystal has a random value from 1- 12 and assign to it
-    var random = Math.floor(Math.random() * 11) + 1;
+    var random = Math.floor(Math.random() * 12) + 1;
     //Assigning random number to crystal
     $(this).attr("data-random", random);
   });
 }
+
 //Start the game with 0 wins and losses
 function startGame() {
+  var yourScore = 0;
   $(".wins").html("<h3>Wins: </h3>" + wins);
   $(".losses").html("<h3>Losses: </h3>" + lost);
-
   getnewRandomnumbers();
+}
+function winner() {
+  wins = wins++;
+  $("#wins").html("Wins: " + wins);
+  alert("YOU WIN!");
+  $("#yourScore").html(yourScore);
+  console.log("YOU WIN");
+  startGame();
+}
+function loser() {
+  losses = lost++;
+  $("#losses").html("Losses: " + lost);
+  alert("You lost, try again. Good luck!");
+  $("#yourScore").html(yourScore);
+  console.log("You lost");
+  getnewRandomnumbers();
+  startGame();
 }
 //When a crystal image gets clicked
 $(".crystal-image").on("click", function() {
@@ -38,19 +56,12 @@ $(".crystal-image").on("click", function() {
   yourScore += crystalValue;
   //Check if it's a win or loss after this click
   if (yourScore === randomNum) {
-    $("#wins").html("<h3>Wins: </h3>" + wins);
-    alert("YOU WIN!");
-    getnewRandomnumbers();
+    winner();
   } else if (yourScore > randomNum) {
-    $("#losses").html("<h3>Losses: </h3>");
-    alert("You lost, try again. Good luck!");
-    getnewRandomnumbers();
+    loser();
   } else {
     //Put your score on the screen
-    $("#yourScore").html("Your Score: " + yourScore);
-
+    $("#yourScore").html("Your New Score: " + yourScore);
     console.log("Your new Score: " + yourScore);
   }
 });
-
-/// */
